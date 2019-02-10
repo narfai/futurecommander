5bed0dd4398a017e6c0218ba3aa30e509889d4a9
François Cadeillan <francois@azsystem.fr>
2019-02-11 00:48:47 +0100

    shell demo with clap; ls / cd / cwd

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,68 @@
+#[macro_use]
+extern crate clap;
+
+use std::path::{ PathBuf, Path, Component };
+use std::fs::{ ReadDir, DirEntry };
 use std::io::{stdin, stdout};
 
+use clap::App;
+use std::env;
+mod path;
+use path::absolute;
+
+
+
+
+fn ls(path: PathBuf) {
+    path.as_path().read_dir()
+        .and_then(|results: ReadDir| {
+            for result in results {
+                println!("{:?}", result.unwrap());
+            }
+            Ok(())
+        });
+}
+
+
 fn main() {
+    let yaml = load_yaml!("clap.yml");
+    let mut cwd: PathBuf = env::current_dir().unwrap();
+
     loop {
         print!("> ");
-
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
         let command = input.trim();
+        let mut argv = vec!["futurecommander"];
+        argv.extend(command.split(" "));
 
-        println!("{}", command);
+        match App::from_yaml(yaml).get_matches_from_safe(argv) {
+            Ok(matches) => {
+                if let Some(matches) = matches.subcommand_matches("ls") {
+                    let path = PathBuf::from(matches.value_of("path").unwrap_or(cwd.to_str().unwrap()));
+
+                    ls(absolute(&cwd.as_path(), &path).to_path_buf());
+                } else if let Some(matches) = matches.subcommand_matches("cwd") {
+                    println!("{:?}", cwd)
+                } else if let Some(matches) = matches.subcommand_matches("cd") {
+                    let path = Path::new(matches.value_of("path").unwrap());
+
+                    if path.exists() && path.is_dir() {
+                        cwd = absolute(&cwd.as_path(), &path).to_path_buf();
+                        println!("{:?}", cwd);
+                    } else {
+                        println!("Target does not exists or is not a directory");
+                    }
+                } else {
+                    println!("Unknown command");
+                }
+            },
+            Err(error) => {
+                println!("{}", error);
+            }
+        }
     }
 }
+
+
---
+++ src/path.rs
@@ -0,0 +1,57 @@
+use std::path::Path;
+use std::path::PathBuf;
+use std::path::Component;
+
+pub fn absolute(cwd: &Path, path: &Path) -> PathBuf {
+    normalize(&cwd.join(path) )
+}
+
+#[test]
+fn test_absolute() {
+    use std::env;
+    let cwd: Path = env::current_dir().unwrap().as_path();
+    assert_eq!(absolute(&cwd, Path::from("././")).is_absolute(), true);
+    assert_eq!(absolute(&cwd, Path::from("/./")).is_absolute(), true);
+    assert_eq!(absolute(&cwd, Path::from("../")).is_absolute(), true);
+}
+
+/**
+* Thanks to ThatsGobbles ( https://github.com/ThatsGobbles ) for his solution : https://github.com/rust-lang/rfcs/issues/2208
+* This code will be removed when os::make_absolute will be marked as stable
+*/
+pub fn normalize(p: &Path) -> PathBuf {
+    let mut stack: Vec<Component> = vec![];
+
+    for component in p.components() {
+        match component {
+            Component::CurDir => {},
+            Component::ParentDir => {
+                match stack.last().cloned() {
+                    Some(c) => {
+                        match c {
+                            Component::Prefix(_) => { stack.push(component); },
+                            Component::RootDir => {},
+                            Component::CurDir => { unreachable!(); },
+                            Component::ParentDir => { stack.push(component); },
+                            Component::Normal(_) => { let _ = stack.pop(); }
+                        }
+                    },
+                    None => { stack.push(component); }
+                }
+            },
+            _ => { stack.push(component); },
+        }
+    }
+
+    if stack.is_empty() {
+        return PathBuf::from(".");
+    }
+
+    let mut norm_path = PathBuf::new();
+
+    for item in &stack {
+        norm_path.push(item);
+    }
+
+    norm_path
+}
