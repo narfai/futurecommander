de05b29e527e0833c9635a554749c29f9829b7be
Francois Cadeillan <francois@azsystem.fr>
2021-07-22 20:48:22 +0200

    Session 3 : Directory nodes are tree containing children's nodes

--- tdd/src/main.rs
+++ tdd/src/main.rs
@@ -7,85 +7,103 @@ fn main() {
     println!("Hello, world!");
 }
 
-// INNER EDGE
-pub struct DirEntryBridge {
-    name: String, 
-    is_dir: bool
-}
+/**
+ * STD -> INNER EDGE ( INPUT ) -> ( BUSINESS -> STRING ) -> INNER EDGE ( OUTPUT )
+ */
 
-impl DirEntryBridge {
-    pub fn file_name(&self) -> &str {
-        &self.name
+#[cfg(test)]
+mod test {
+    trait DirEntry {
+        fn file_name(&self) -> &str;
+        fn is_dir(&self) -> bool;
     }
 
-    pub fn is_dir(&self) -> bool {
-        self.is_dir
+    // INNER EDGE
+    pub struct DirEntryStub {
+        name: String, 
+        is_dir: bool
     }
-}
 
-// BUSINESS
-#[derive(Debug, Clone)]
-pub enum Node {
-    Directory {
-        name: String
-    },
-    File {
-        name: String
-    }
-}
+    impl DirEntry for DirEntryStub {
+        fn file_name(&self) -> &str {
+            &self.name
+        }
 
-impl Node {
-    pub fn from(dir_entry: DirEntryBridge) -> Self {
-        if dir_entry.is_dir() {
-            Node::Directory { name: dir_entry.file_name().to_owned() }
-        } else {
-            Node::File { name: dir_entry.file_name().to_owned() }
+        fn is_dir(&self) -> bool {
+            self.is_dir
         }
     }
-}
 
-//Equivalent de #[derive(Eq)]
-impl PartialEq for Node {
-    fn eq(&self, other: &Self) -> bool {
-        match self {
-            Node::Directory{ name: self_name } => 
-                match other {
-                    Node::Directory{ name: other_name } => self_name == other_name,
-                    Node::File { name: _ } => false 
-                },
-            Node::File{ name: self_name } => 
-                match other {
-                    Node::File{ name: other_name } => self_name == other_name,
-                    Node::Directory { name: _ }=> false
-                }
+    // BUSINESS
+    #[derive(PartialEq)]
+    pub enum Node {
+        Directory {
+            name: String,
+            children: Vec<Node> 
+        },
+        File {
+            name: String
         }
     }
-}
 
-#[test]
-fn test_extract_node_from_direntry_is_a_directory() {
-    let dir_entry = DirEntryBridge {
-        name: String::from("A"),
-        is_dir: true
-    };
+    impl Node {
+        pub fn from(dir_entry: DirEntryStub) -> Self {
+            if dir_entry.is_dir() {
+                Node::Directory { name: dir_entry.file_name().to_owned(), children: Vec::new() } 
+            } else {
+                Node::File { name: dir_entry.file_name().to_owned() }
+            }
+        }
+    }
 
-    let expected = Node::Directory {
-        name: String::from("A") 
-    };
-    
-    assert!(Node::from(dir_entry) == expected);
-}
+    #[test]
+    fn test_extract_node_from_direntry_is_a_directory() {
+        let dir_entry = DirEntryStub {
+            name: String::from("A"),
+            is_dir: true
+        };
 
-#[test]
-fn test_extract_node_from_direntry_is_a_file() {
-    let dir_entry = DirEntryBridge {
-        name: String::from("F"),
-        is_dir: false
-    };
+        let expected = Node::Directory {
+            name: String::from("A"),      
+            children: Vec::new()
+        };
+        
+        assert!(Node::from(dir_entry) == expected);
+    }
 
-    let expected = Node::File {
-        name: String::from("F") 
-    };
+    #[test]
+    fn test_extract_node_from_direntry_is_a_file() {
+        let dir_entry = DirEntryStub {
+            name: String::from("F"),
+            is_dir: false
+        };
 
-    assert!(Node::from(dir_entry) == expected);
+        let expected = Node::File {
+            name: String::from("F") 
+        };
+
+        assert!(Node::from(dir_entry) == expected);
+    }
+
+    /*
+    DRAFT
+    We need to represents the children of a node because we need to display ordered list with ls
+    Ls prends un Path en argument
+    Node doit être capable de représenter un arbre de donnée
+    Node + Path => Node ( enfant )
+    */
+
+    // TODO
+    // https://doc.rust-lang.org/std/fs/struct.ReadDir.html
+    // pub struct ReadDirBridge<'a> {}
+    // ReadDirBridge<'a> => NodeIterator<'a>
+
+    // TODO ABSTRAIRE L'OUTPUT
+    // Définir l'abstraction d'UI => trait / générique
+
+    // TODO ABSTRAIRE L'INPUT
+    // Définir l'abstraction pour wrapper les ReadDir et les DirEntry
+    // https://doc.rust-lang.org/std/os/unix/fs/trait.DirEntryExt.html
+    // futurecommander::ReadDirExt
+    // futurecommander::DirEntryExt
 }
